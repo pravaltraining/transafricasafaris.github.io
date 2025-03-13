37e5c17ccab41beaf232ade296698f3a03940996
govardhanudumula <157101817+govardhanudumula@users.noreply.github.com>
2025-03-13 14:54:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+function validateForm() {
+    // let firstName = document.getElementById("firstName").value;
+    // let lastName = document.getElementById("lastName").value;
+    // let email = document.getElementById("email").value;
+    // let phone = document.getElementById("phone").value;
+
+    // if (firstName === "" || lastName === "" || email === "" || phone === "") {
+    //     alert("Please fill out all required fields.");
+    //     return false;
+    // }
+    // let pdfUrl = "Images/blank.pdf";
+    // let link = document.createElement("a");
+    // link.href = pdfUrl;
+    // link.download = "Itinerary.pdf";
+    // document.body.appendChild(link);
+    // link.click();
+    return true;
+}
+
